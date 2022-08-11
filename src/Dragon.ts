c7fd81737bfa2073f887ded442c67d328f94123d
cassiusbessa <cassiusbessa@gmail.com>
2022-08-10 21:26:40 -0300

    atributos do dragão definidos melhor

--- src/Dragon.ts
+++ src/Dragon.ts
@@ -2,7 +2,7 @@ import Monster from './Monster';
 import getRandomInt from './utils';
 
 export default class Dragon extends Monster {
-  constructor(lifePoints = 1000, strength = 100) {
+  constructor(lifePoints = 100, strength = 50) {
     super(lifePoints * getRandomInt(10, 50), strength * getRandomInt(1, 10));
   }
 }
